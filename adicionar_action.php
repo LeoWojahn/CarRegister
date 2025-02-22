3a0d96a904592b141a0862c5efc00e05e4eff898
Leo Wojahn <leowojahn@gmail.com>
2025-02-22 12:54:55 -0300

    vs3
    Feito:
    -Todo o adicionar_action está pronto. Falta apenas construir a função adicionarAction para de fato adicionar as informações em um json.

--- adicionar_action.php
+++ adicionar_action.php
@@ -2,10 +2,35 @@
 
 session_start();
 
+require_once "class/Car.php";
+require_once "functions/adicionarAction.php";
+
 if($_SERVER["REQUEST_METHOD"] == "POST") {
 
+    $carro = new Car;
+
+    $carro->setFab(htmlspecialchars($_POST['fabrica']));
+    $carro->setModel(htmlspecialchars($_POST['modelo']));
+    $carro->setVersion(htmlspecialchars($_POST['versao']));
+    $carro->setYear(htmlspecialchars(intval($_POST['ano'])));
+    $carro->setColor(htmlspecialchars($_POST['cor']));
+
+    if(!empty($carro->getFab() && $carro->getModel() && $carro->getVersion() && $carro->getYear() && $carro->getColor())) {
+      
+        adicionarAction();
+
+    } else {
+
+        $_SESSION['error_message'] = "Erro: preencha todos os dados.";    
+        header("Location: index.php");
+        exit();
+
+    }
+
 } else {
+
     $_SESSION['error_message'] = "Erro: método enviado pelo formulário não aceito.";    
     header("Location: index.php");
     exit();
+
 }
