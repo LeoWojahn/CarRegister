5e1d9b602a19e0f45034ff44034a721f35698577
Leo Wojahn <leowojahn@gmail.com>
2025-02-24 21:14:53 -0300

    vs5
    Feito:
    -Tabela sendo exibida.

---
+++ includes/table.php
@@ -0,0 +1,65 @@
+<?php 
+
+require_once('functions/fabs.php');
+
+?>
+
+<table border="1">
+        <thead>
+            <tr>
+                <form action="adicionar_action.php" method="post">
+                    <td>
+                        <select name="fabrica">
+                            <?php fabs(); ?>
+                        </select>
+                    </td>
+                    <td>
+                        <input type="text" name="modelo">
+                    </td>
+                    <td>
+                        <input type="text" name="versao">
+                    </td>
+                    <td>
+                        <input type="number" name="ano">
+                    </td>
+                    <td>
+                        <input type="text" name="cor">
+                    </td>
+                    <td>
+                        <input type="submit" value="Adicionar">
+                    </td>
+                </form>
+            </tr>
+            <tr>
+                <th>Fábrica</th>
+                <th>Modelo</th>
+                <th>Versão</th>
+                <th>Ano Fabricação</th>
+                <th>Cor</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            <?php
+            
+            $arq = "data/cars.json";
+
+            if(file_exists($arq)) {
+                $arq = file_get_contents($arq);
+                $infos = json_decode($arq, true);
+            }
+
+            foreach($infos as $info) : 
+            
+            ?>
+            <tr>
+                <td><?=$info["fab"];?></td>
+                <td><?=$info["model"];?></td>
+                <td><?=$info["version"];?></td>
+                <td><?=$info["year"];?></td>
+                <td><?=$info["color"];?></td>
+            </tr>
+            <?php endforeach; ?>
+
+        </tbody>
+    </table>
--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 
 session_start();
 
-require_once('functions/fabs.php');
-
 ?>
 
 <!DOCTYPE html>
@@ -14,55 +12,14 @@ require_once('functions/fabs.php');
     <title>Car</title>
 </head>
 <body>
+
     <?php
     if(isset($_SESSION['error_message'])) {
         echo '<p style="color: red;">' . $_SESSION['error_message'] . '</p>';
         unset($_SESSION['error_message']);
     }
     ?>
-    <table border="1">
-        <thead>
-            <tr>
-                <form action="adicionar_action.php" method="post">
-                    <td>
-                        <select name="fabrica">
-                            <?php fabs(); ?>
-                        </select>
-                    </td>
-                    <td>
-                        <input type="text" name="modelo">
-                    </td>
-                    <td>
-                        <input type="text" name="versao">
-                    </td>
-                    <td>
-                        <input type="number" name="ano">
-                    </td>
-                    <td>
-                        <input type="text" name="cor">
-                    </td>
-                    <td>
-                        <input type="submit" value="Adicionar">
-                    </td>
-                </form>
-            </tr>
-            <tr>
-                <th>Fábrica</th>
-                <th>Modelo</th>
-                <th>Versão</th>
-                <th>Ano Fabricação</th>
-                <th>Cor</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Fab</td>
-                <td>Model</td>
-                <td>Version</td>
-                <td>Year</td>
-                <td>Color</td>
-            </tr>
-        </tbody>
-    </table>
+
+    <?php include_once("includes/table.php"); ?>
 </body>
 </html>
